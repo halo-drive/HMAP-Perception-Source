269bd970a3f46642a0c2fdc4f495e6a60dd87ae0
atharvpomo <atharv.sharma@pomorobotics.com>
2026-02-17 16:59:31 +0000

    added aarch library support in cmake and updates for eigen fixes

--- standaloneapps/slam/dw_fastlio/include/fix_eigen_x11.h
+++ standaloneapps/slam/dw_fastlio/include/fix_eigen_x11.h
@@ -4,12 +4,32 @@
 // This header undefines Success before Eigen headers are included
 /////////////////////////////////////////////////////////////////////////////////////////
 
+// #ifndef FIX_EIGEN_X11_H_
+// #define FIX_EIGEN_X11_H_
+
+// #ifdef Success
+// #undef Success
+// #endif
+
+// #endif // FIX_EIGEN_X11_H_
+
 #ifndef FIX_EIGEN_X11_H_
 #define FIX_EIGEN_X11_H_
+
+#if __has_include(<X11/Xlib.h>)
+#include <X11/Xlib.h>
+#endif
+
+#if __has_include(<X11/Xlib.h>)
+#include <X11/Xlib.h>
+#endif
 
 #ifdef Success
 #undef Success
 #endif
 
-#endif // FIX_EIGEN_X11_H_
+#ifdef Status
+#undef Status
+#endif
 
+#endif // FIX_EIGEN_X11_H_
---
+++ standaloneapps/slam/readme.txt
@@ -0,0 +1,4 @@
+please find required aarch libraries from here: 
+
+
+https://drive.google.com/drive/folders/1tQX0xWDk1i_a3UyBH1P6r5owxy9kuDjL?usp=drive_link
